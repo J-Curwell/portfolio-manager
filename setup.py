7cc2cbc3b4e26a2d573e4589d48eaf54dd7f0de3
James Curwell <JamesCurwell97@gmail.com>
2021-01-31 23:08:44 +0000

    Move source code into src dir and some setup.py changes

--- setup.py
+++ setup.py
@@ -6,17 +6,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="portfolio-manager",
     version="0.0.1",
+    license="MIT",
     author="James Curwell -- George Calvert",
     author_email="jamescurwell97@gmail.com",
     description="Simple tool for monitoring the performance of investment portfolios.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/J-Curwell/portfolio-manager",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    test_suite="tests"
 )
